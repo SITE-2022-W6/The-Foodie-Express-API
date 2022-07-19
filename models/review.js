4b8b638ef8960137a8c708a4bf190b5bc8057e76
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-07-19 17:54:55 -0400

    Implemented getReviewById method

--- models/review.js
+++ models/review.js
@@ -2,7 +2,7 @@ class Review {
     static async createReview(content) {
         const requiredFields = ["user_id", "restaurant_id", "menu_item_name", "rating"];
         requiredFields.forEach(field => {
-            if (!data.hasOwnProperty(field)){
+            if (!content.hasOwnProperty(field)){
                 throw new BadRequestError(`Missing ${field} in req body`)
             }
         })
@@ -15,12 +15,22 @@ class Review {
             [content.id, content.user_id, content.menu_item_name, content.rating]
         )
 
-
         return result.rows[0]
     }
 
-    static async getReviewById() {
+    static async getReviewById(reqBody) {
+        const id = reqBody.id
 
+        if(!id) {
+            throw new BadRequestError(`Missing id in req body`)
+        }
+
+        const result = await db.query(
+            `SELECT * FROM reviews WHERE id=$1;`,
+            [id]
+        )
+
+        return result.rows[0]
     }
 }
 
