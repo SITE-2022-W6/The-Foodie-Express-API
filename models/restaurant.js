db23f347c246e8c9d9e251328882877dd755c219
SSalto <73043956+StevenSalto@users.noreply.github.com>
2022-07-23 18:21:06 -0400

    Rewrote table schema for menus and restaurants. Added items table. Updated getMenuByRestaurantName in restaurant model.

--- models/restaurant.js
+++ models/restaurant.js
@@ -25,9 +25,11 @@ class Restaurant {
     }
     static async apiCallForRestaurantByName(restaurantName, city, postal_code) {
         const location = ''
+        // Check for city or postal_code, throw an error if none was passed
         if(city=='' && postal_code==0) {
             throw new BadRequestError('CITY OR POSTAL CODE MISSING')
         }
+        // Determine which parameter to use for api call
         if(city!='') {
             location = 'city'
         }
@@ -35,15 +37,83 @@ class Restaurant {
             location = 'postal_code'
         }
 
-        return axios.get(`https://openmenu.com/api/v2/location.php?key=${OM_API_KEY}&country=us&${location}=${location=='city'?city:postal_code}&s=${restaurantName}`)
-                    .catch((err) => {
-                        console.log(err)
-                    })
+        const result = await axios.get(`https://openmenu.com/api/v2/location.php?key=${OM_API_KEY}&country=us&${location}=${location=='city'?city:postal_code}&s=${restaurantName}`)
+            .catch((err) => {
+                console.log(err)
+            })
+        
+        return result.data.response.result
     }
-    static async addRestaurantToDb(apiCallResponse) {
+    static async addRestaurantToDb(data) {
+        // Set restaurant values
+        let OpenMenu_id = data.id
+        let name = data.restaurant_info.restaurant_name
+        let brief_description = data.restaurant_info.brief_description
+        let phone = data.restaurant_info.phone
+        let fax = data.restaurant_info.fax
+        let address_1 = data.restaurant_info.address_1
+        let address_2 = data.restaurant_info.address_2
+        let cuisine_type_primary = data.environment_info.cuisine_type_primary
+        let operating_days = JSON.stringify(data.operating_days)
+        let operating_days_printable = data.JSON.stringify(data.operating_days_printable)
+        let restaurant_verbose = data
 
+        //Insert restaurant into db
+        const dbResponse = await db.query(`
+        INSERT INTO restaurants (
+            OpenMenu_id,
+            name,
+            brief_description,
+            phone, 
+            fax,
+            address_1, 
+            address_2, 
+            cuisine_type_primary, 
+            operating_days,
+            operating_days_printable,
+            restaurant_verbose )
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+        RETURNING *`, 
+        [id, OpenMenu_id, name, brief_description, phone, fax, address_1, address_2, cuisine_type_primary, operating_days, operating_days_printable, restaurant_verbose])
+
+        this.addMenusToDb(data.menus, dbResponse.rows[0].id)
+
+        return dbResponse.rows[0]
     }
+    static async addMenusToDb(menus, restaurant_id) {
+        menus.forEach((menu) => { 
+            // Set menu values
+            let name = menu.menu_name
+            let descrption = menu.menu_description
+            let menu_verbose = menu
+            // Insert menu into db
+            const dbResponse = await db.query(`
+            INSERT INTO menus (restaurant_id, menu_name, menu_description, menu_verbose)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *`, 
+            [restaurant_id, name, descrption, menu_verbose])
 
+            this.addItemsToDb(menu.menu_groups, dbResponse.rows[0].id)
+         })
+    }
+    static async addItemsToDb(groups, menu_id) {
+        groups.forEach((group) => {
+            let group_name = group.group_name
+            group.menu_items.forEach((item) => {
+                let name = item.menu_item_name
+                let description = item.menu_item_descrption
+                let price = item.menu_item_price
+                let calories = item.menu_item_calories
+                let item_verbose = item
+
+                const dbResponse = await db.query(`
+                INSERT INTO items (menu_id, group_name, name, description, price, calories, item_verbose)
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
+                RETURNING *`, 
+                [menu_id, group_name, name, description, price, calories, item_verbose])
+            })
+        })
+    }
     //Gets a restaurant by its OpenMenu id
     static async getRestaurantById(id) {
         //First check the database to see if it is stored
@@ -153,12 +223,7 @@ class Restaurant {
 
     }
 
-    static async getMenuItemByRestaurantId() {
-
-    }
-    // Returns an array of menu items given OpenMenu API response data
-    static async getMenuItems(data) {
-        const data = response.data.response.result
+    static async getMenuItemsByRestaurantId() {
 
     }
 }
