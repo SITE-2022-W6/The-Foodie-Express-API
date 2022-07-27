8e18a9987da4a947cc887d8a320343b12325c802
WinsonChen114 <14winsonchen@gmail.com>
2022-07-27 12:40:50 -0400

    Fix incorrect return when looking for a menu

--- models/menu.js
+++ models/menu.js
@@ -4,28 +4,44 @@ const { NotFoundError } = require('../utils/errors')
 
 class Menu {
     static async insertMenu(menus, restaurant_id) {
-        menus.forEach(async (menu) => { 
+        //Returns nested array of Promises
+        const Promises = await menus.map(async (menu) => { 
             // Set menu values
             let name = menu.menu_name
-            let descrption = menu.menu_description
+            let description = menu.menu_description
             let menu_verbose = menu
             // Insert menu into db
             const dbResponse = await db.query(`
             INSERT INTO menus (restaurant_id, menu_name, menu_description, menu_verbose)
             VALUES ($1, $2, $3, $4)
             RETURNING *`, 
-            [restaurant_id, name, descrption, menu_verbose])
+            [restaurant_id, name, description, menu_verbose])
 
-            this.addItemsToDb(menu.menu_groups, dbResponse.rows[0].id)
+            const menuGroup = await this.addItemsToDb(menu.menu_groups, dbResponse.rows[0].id)
+            return menuGroup
          })
+         //Waits for promises to resolve. Nested Array of menu items
+         const menuNestedArr = await Promise.all(Promises)
+
+         //Rearranges menu items into 1 array
+         let menuArr = []
+         menuNestedArr.forEach((menu) => {
+            menu.forEach((group) => {
+                group.forEach((item) => {
+                    menuArr.push(item)
+                })
+            })
+         })
+         return menuArr
     }
     
     static async addItemsToDb(groups, menu_id) {
-        groups.forEach((group) => {
+        const returnGroup = await groups.map(async (group) => {
+
             let group_name = group.group_name
-            group.menu_items.forEach(async (item) => {
+            const groupMenuItems = await group.menu_items.map(async (item) => {
                 let name = item.menu_item_name
-                let description = item.menu_item_descrption
+                let description = item.menu_item_description
                 let price = item.menu_item_price
                 let calories = item.menu_item_calories
                 let item_verbose = item
@@ -33,10 +49,15 @@ class Menu {
                 const dbResponse = await db.query(`
                 INSERT INTO items (menu_id, group_name, name, description, price, calories, item_verbose)
                 VALUES ($1, $2, $3, $4, $5, $6, $7)
-                RETURNING *`, 
+                RETURNING id, name, group_name, description, price, calories`, 
                 [menu_id, group_name, name, description, price, calories, item_verbose])
+
+                
+                return dbResponse.rows[0]
             })
+            return await Promise.all(groupMenuItems)
         })
+        return Promise.all(returnGroup)
     }
 
     static async getMenuBy(restaurantId) {
