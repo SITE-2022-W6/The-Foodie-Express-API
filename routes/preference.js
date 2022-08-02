c5be5f52ae309465e25f55e723fd5dbb37b96836
Steven Salto <ssalto2702@gmail.com>
2022-08-02 18:35:25 -0400

    Preference model and routes refactor

---
+++ routes/preference.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const Preference = require("../models/preference")
+
+router.post('/set-preference', async (req, res, next) => {
+    try {
+        const preference = await Preference.setPreference(parseInt(req.body.userId), req.body.cuisine, parseInt(req.body.rating))
+        res.status(200).json( preference )
+    } catch(e) {
+        next(e)
+    }
+})
+
+router.get('/favorite-cuisine/:id', async (req, res, next) => {
+    try {
+        const favoriteCuisine = await Preference.favoriteCuisine(parseInt(req.params.id))
+        res.status(200).json( favoriteCuisine )
+    } catch(e) {
+        next(e)
+    }
+})
+
+module.exports = router
