ad6c65b0fe2d677f132d1da75e22c8527f60088e
WinsonChen114 <14winsonchen@gmail.com>
2022-08-10 15:12:01 -0700

    Commented out console logs

--- models/review.js
+++ models/review.js
@@ -44,18 +44,18 @@ class Review {
              RETURNING *`,
             values
         )
-        console.log(parseInt(result.rows[0].rest_id))
+        // console.log(parseInt(result.rows[0].rest_id))
         const restPrimCuis = await db.query(
             `SELECT id, name, cuisine_type_primary
             FROM restaurants 
             WHERE id=$1`, 
             [parseInt(result.rows[0].rest_id)]
         )
-        console.log("primCuis", restPrimCuis.rows[0].cuisine_type_primary)
+        // console.log("primCuis", restPrimCuis.rows[0].cuisine_type_primary)
         let pref = await Preference.setPreference(result.rows[0].user_id, restPrimCuis.rows[0].cuisine_type_primary, result.rows[0].rating)
 
 
-        console.log(pref)
+        // console.log(pref)
 
 
         // Return our db entry...
